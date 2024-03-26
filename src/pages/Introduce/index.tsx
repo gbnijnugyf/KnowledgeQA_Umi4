dc104b8b49afa1095485ca5dd7dd1a451c0c82c5
gbnijnugyf <1612517397@qq.com>
2024-03-26 14:35:20 +0800

    feat: autoplay intruduce video

--- src/pages/Introduce/index.tsx
+++ src/pages/Introduce/index.tsx
@@ -79,6 +79,7 @@ const IntroduceBackground = () => {
               <video
                 controls
                 width="70%"
+                muted
                 autoPlay
                 loop
                 style={{
